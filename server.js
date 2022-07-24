8c1af5fff975dd3afe8d236f146ae668cab83ad9
Andres Garcia Sanchez <force.rap20@icloud.com>
2022-07-23 21:04:12 -0500

    Fifth commit

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-transparent-81180',
-    user : 'andresgarcia',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
