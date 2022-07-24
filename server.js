3f523b80ac5b8822ef3ba786fd9a98f1a4585d45
Andres Garcia Sanchez <force.rap20@icloud.com>
2022-07-23 22:13:28 -0500

    1

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+
 const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+    rejectUnauthorized: false
   }
 });
 
